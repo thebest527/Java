c5dbd90602c285fdcecf2b64512643470448954d
thebest527 <thebest5277@gmail.com>
2020-04-29 15:03:12 +0900

    2020/04/29 java

---
+++ ch01/src/sub1/Hello.java
@@ -0,0 +1,14 @@
+package sub1;
+/*
+ * 날짜:2020/04/29
+ * 이름:박수령
+ * 자바 HelloWorld 출력하기
+ */
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("Hello Java!");
+	}
+
+}
