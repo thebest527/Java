5c21f9ff3bba3a8d9915d95e92b8d2fa07f2aed2
thebest527 <thebest5277@gmail.com>
2020-05-06 13:54:49 +0900

    2020/05/01 Javatest

---
+++ JavaTest/src/test01/JavaTest.java
@@ -0,0 +1,31 @@
+package test01;
+/* 날짜 : 2020/05/01
+ * 이름 : 박수령
+ * 내용 : 자바 데이터 타입 연습문제
+ */
+public class JavaTest {
+	
+	public static void main(String[] args) {
+		char     ch1 = 'A';
+		byte    var2 = 100;
+		long    var3 = 21474836488L;
+		float   var4 = 3.14f;
+		double  var5 = 3.14159;
+		boolean var6 = true;
+		boolean var7 = false;
+		String  str8 = "Hello";
+		String  str9 = null; //이변수에 데이터가 없다는 의미, 기본타입은 'null'값을 취급 못한다.
+		
+		System.out.println("ch1  : "+ch1);
+		System.out.println("var2 : "+var2);
+		System.out.println("var3 : "+var3);
+		System.out.println("var4 : "+var4);
+		System.out.println("var5 : "+var5);
+		System.out.println("var6 : "+var6);
+		System.out.println("var7 : "+var7);
+		System.out.println("str8 : "+str8);
+		System.out.println("str9 : "+str9);
+		
+	}
+
+}
---
+++ JavaTest/src/test02/Javatest2.java
@@ -0,0 +1,34 @@
+package test02;
+import java.util.Scanner;
+
+/* 날짜 : 2020/05/01
+ * 이름 : 박수령
+ * 내용 : 자바 기본 입출력 연습문제
+ */
+
+public class Javatest2 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		input;
+		System.out.println("input 값 입력 : ");
+		
+		input = sc.nextInt() ;
+		int result = ;
+		
+		System.out.println("(1+2) x input = "+result);
+		
+		name;
+		System.out.println("name 값 입력 : ");
+		
+		name = sc.next();
+		String hello = "안녕 홍길동님 반갑습니다." ;
+		
+		System.out.println(hello);
+		
+		
+	}
+
+}
