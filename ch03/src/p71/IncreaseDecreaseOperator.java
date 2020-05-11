b523e5f0cec2e7cc21fdab6fb767ee69d2d95f38
thebest527 <thebest5277@gmail.com>
2020-05-11 13:51:38 +0900

    Ch03 교재 예제문제

---
+++ ch03/src/p71/IncreaseDecreaseOperator.java
@@ -0,0 +1,39 @@
+package p71;
+// 증감연산자
+public class IncreaseDecreaseOperator {
+
+	public static void main(String[] args) {
+		int x = 10;
+		int y = 10;
+		int z;
+		
+		
+		System.out.println("-----------------------");
+		x++;
+		++x;
+		System.out.println("x=" + x);
+		
+		System.out.println("-----------------------");
+		y--;
+		--y;
+		System.out.println("y=" + y);
+		
+		System.out.println("-----------------------");
+		z = x++;
+		System.out.println("z=" + z);
+		System.out.println("x=" + x);
+		
+		System.out.println("-----------------------");
+		z = ++x;
+		System.out.println("z=" + z);
+		System.out.println("x=" + x);
+		
+		System.out.println("-----------------------");
+		z = ++x + y++;
+		System.out.println("z=" + z);
+		System.out.println("x=" + x);
+		System.out.println("y=" + y);
+			
+	}
+
+}
---
+++ ch03/src/p73/DenyLogicOperator.java
@@ -0,0 +1,19 @@
+package p73;
+// 논리 부정 연산자
+
+public class DenyLogicOperator {
+
+	public static void main(String[] args) {
+		
+		boolean play = true;
+		System.out.println(play);
+		
+		play = !play;
+		System.out.println(play);
+		
+		play = !play;
+		System.out.println(play);
+
+	}
+
+}
